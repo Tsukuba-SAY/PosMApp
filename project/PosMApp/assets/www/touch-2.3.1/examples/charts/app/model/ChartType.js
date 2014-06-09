297328397f8ccbae76a7ebd4f1218f0dfe77d13f
Yuji Imagawa <using923@gmail.com>
2014-06-09 12:24:13 +0900

    Remove resources of Sencha Touch

--- project/PosMApp/assets/www/touch-2.3.1/examples/charts/app/model/ChartType.js
+++
@@ -1,9 +0,0 @@
-Ext.define('Charts.model.ChartType', {
-    extend : 'Ext.data.Model',
-    config : {
-        fields : [
-            { name : 'id',    type : 'int' },
-            { name : 'title', type : 'string' }
-        ]
-    }
-});
