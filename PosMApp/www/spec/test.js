ee3be6ea2691e33afb3a3ecbefa5a98e006b73b5
Shota Furuya <s1420693@u.tsukuba.ac.jp>
2014-06-18 17:53:15 +0900

    Refactor index.html and postermap.js (for unit tests)

--- PosMApp/www/spec/test.js
+++ PosMApp/www/spec/test.js
@@ -17,3 +17,58 @@ describe("postermap.jsの単体テスト", function() {
 		});
 	});
 });
+
+describe("searchByTitleの単体テスト", function() {
+	it("全件ヒットする", function() {
+		var posterids, flag;
+
+		runs(function() {
+			posterids = searchByTitle("の");	
+		});
+
+		waits(100);
+
+		runs(function() {
+			expect(posterids.length).toEqual(10);
+		});
+	});
+	it("1件だけヒットする", function() {
+		var posterids, flag;
+
+		runs(function() {
+			posterids = searchByTitle("Twitter");	
+		});
+
+		waits(100);
+
+		runs(function() {
+			expect(posterids.length).toEqual(1);
+		});
+	});
+	it("1件もヒットしない", function() {
+		var posterids, flag;
+
+		runs(function() {
+			posterids = searchByTitle("ほげほげ");	
+		});
+
+		waits(100);
+
+		runs(function() {
+			expect(posterids.length).toEqual(0);
+		});
+	});
+	it("Case-insensitiveな検索ができる", function() {
+		var posterids, flag;
+
+		runs(function() {
+			posterids = searchByTitle("twitter");	
+		});
+
+		waits(100);
+
+		runs(function() {
+			expect(posterids.length).toEqual(1);
+		});
+	});
+});
