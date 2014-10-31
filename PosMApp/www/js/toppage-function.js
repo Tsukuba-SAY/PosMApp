f1360fa4f779f6243de27a342088224a22509796
Shota Furuya <s1420693@u.tsukuba.ac.jp>
2014-10-31 12:40:41 +0900

    詳細情報画面の戻るボタンを押した時の挙動を変更

--- PosMApp/www/js/toppage-function.js
+++ PosMApp/www/js/toppage-function.js
@@ -20,11 +20,12 @@ $.fn.jumpToDetailPage = function() {
 // 詳細情報画面の戻るボタンをおした時の挙動
 $.fn.backToPreviousPage = function() {
 	$(this).on("touchstart", function(e) {
-		var prev = sessionStorage.getItem("previousPage");
-		if (prev == null || prev == undefined) {
-			prev = "posterMapPage";
-		}
-		window.location.href = "#" + prev;
+		// var prev = sessionStorage.getItem("previousPage");
+		// if (prev == null || prev == undefined) {
+		// 	prev = "posterMapPage";
+		// }
+		// window.location.href = "#" + prev;
+		window.history.back();
 	});
 }
 
