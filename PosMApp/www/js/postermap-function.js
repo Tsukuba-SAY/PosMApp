e6542a49465ac93f40d0f01c727d573c2895bb39
Shota Furuya <s1420693@u.tsukuba.ac.jp>
2014-11-07 14:57:49 +0900

    一応動いた

--- PosMApp/www/js/postermap-function.js
+++ PosMApp/www/js/postermap-function.js
@@ -540,8 +540,9 @@ function searchChanged(bar) {
 function windowManager () {
 	var STATIC_WIDTH =  600;
 	var scale = window.innerWidth / STATIC_WIDTH;
+
 	var $mapMain = $('#mapMain');
-	$mapMain.css("zoom",scale);
+	$mapMain.css("zoom", scale);
 
     var reqAnimationFrame = (function () {
         return window[Hammer.prefixed(window, 'requestAnimationFrame')] || function (callback) {
@@ -549,68 +550,77 @@ function windowManager () {
         };
     })();
 
-    console.log(reqAnimationFrame);
+    //$(element).hammer(options).bind("pan", myPanHandler);
 
-    var el = document.querySelector("#mapMain");
-    console.log(el);
+    var el = $("#mapMain")[0];
 
-    var START_X = Math.round((window.innerWidth - el.offsetWidth) / 2);
-    var START_Y = Math.round((window.innerHeight - el.offsetHeight) / 2);
-    console.log(START_X + "," + START_Y)
+    // var START_X = Math.round((window.innerWidth - el.offsetWidth) / 2);
+    // var START_Y = Math.round((window.innerHeight - el.offsetHeight) / 2);
+    var START_X = 0;
+    var START_Y = 0;
 
     var ticking = false;
     var transform;
     var timer;
 
-    var mc = new Hammer.Manager(el);
+    var mc = new Hammer.Manager($("#mapFrame")[0]);
 
-    mc.add(new Hammer.Pan({ threshold: 0, pointers: 0 }));
-
-    mc.add(new Hammer.Swipe()).recognizeWith(mc.get('pan'));
-    mc.add(new Hammer.Rotate({ threshold: 0 })).recognizeWith(mc.get('pan'));
-    mc.add(new Hammer.Pinch({ threshold: 0 })).recognizeWith([mc.get('pan'), mc.get('rotate')]);
-
-    mc.add(new Hammer.Tap({ event: 'doubletap', taps: 2 }));
-    mc.add(new Hammer.Tap());
+    mc.add(new Hammer.Pan({ threshold: 0, pointers: 1 }));
+    // mc.add(new Hammer.Swipe()).recognizeWith(mc.get('pan'));
+    // mc.add(new Hammer.Rotate({ threshold: 0 })).recognizeWith(mc.get('pan'));
+    // mc.add(new Hammer.Pinch({ threshold: 0 })).recognizeWith(mc.get('rotate'));
+    mc.add(new Hammer.Pinch());
+    // mc.add(new Hammer.Tap({ event: 'doubletap', taps: 2 }));
+    // mc.add(new Hammer.Tap());
 
     mc.on("panstart panmove", onPan);
-    mc.on("rotatestart rotatemove", onRotate);
+    // mc.on("rotatestart rotatemove", onRotate);
     mc.on("pinchstart pinchmove", onPinch);
-    mc.on("swipe", onSwipe);
-    mc.on("tap", onTap);
-    mc.on("doubletap", onDoubleTap);
+    // mc.on("swipe", onSwipe);
+    // mc.on("tap", onTap);
+    // mc.on("doubletap", onDoubleTap);
 
+    el.className = 'animate';
+    transform = {
+        translate: { x: START_X, y: START_Y },
+        scale: 1
+    };
 
     function resetElement() {
-        el.className = 'animate';
-        transform = {
-            translate: { x: START_X, y: START_Y },
-            scale: 1,
-            angle: 0,
-            rx: 0,
-            ry: 0,
-            rz: 0
-        };
-
-          }
+        // el.className = 'animate';
+        // transform = {
+        //     translate: { x: START_X, y: START_Y },
+        //     scale: 1
+        // };
+    }
 
     function updateElementTransform() {
+        var value = [
+        	'translate(' + transform.translate.x + 'px, ' + transform.translate.y + 'px)',
+        	'scale(' + transform.scale + ', ' + transform.scale + ')'
+        ];
+
+        value = value.join(" ");
+        //el.textContent = value;
+        el.style.webkitTransform = value;
+        el.style.mozTransform = value;
+        el.style.transform = value;
         ticking = false;
     }
 
     function requestElementUpdate() {
         if(!ticking) {
             reqAnimationFrame(updateElementTransform);
-            ticking = true;
+            ticking = false;
         }
     }
-
     
     function onPan(ev) {
+    	console.log("pan");
         el.className = '';
         transform.translate = {
-            x: START_X + ev.deltaX,
-            y: START_Y + ev.deltaY
+            x: ev.deltaX,
+            y: ev.deltaY
         };
 
         requestElementUpdate();
@@ -618,6 +628,7 @@ function windowManager () {
 
     var initScale = 1;
     function onPinch(ev) {
+    	console.log("pinch");
         if(ev.type == 'pinchstart') {
             initScale = transform.scale || 1;
         }
@@ -628,54 +639,58 @@ function windowManager () {
         requestElementUpdate();
     }
 
-    var initAngle = 0;
-    function onRotate(ev) {
-        if(ev.type == 'rotatestart') {
-            initAngle = transform.angle || 0;
-        }
+    // var initAngle = 0;
+    // function onRotate(ev) {
+    // 	console.log("rotate");
+    //     // if(ev.type == 'rotatestart') {
+    //     //     initAngle = transform.angle || 0;
+    //     // }
 
-        el.className = '';
-        transform.rz = 1;
-        transform.angle = initAngle + ev.rotation;
-        requestElementUpdate();
-    }
+    //     // el.className = '';
+    //     // transform.rz = 1;
+    //     // transform.angle = initAngle + ev.rotation;
+    //     // requestElementUpdate();
+    // }
 
-    function onSwipe(ev) {
-        var angle = 50;
-        transform.ry = (ev.direction & Hammer.DIRECTION_HORIZONTAL) ? 1 : 0;
-        transform.rx = (ev.direction & Hammer.DIRECTION_VERTICAL) ? 1 : 0;
-        transform.angle = (ev.direction & (Hammer.DIRECTION_RIGHT | Hammer.DIRECTION_UP)) ? angle : -angle;
+    // function onSwipe(ev) {
+    // 	console.log("swipe");
+    //     // var angle = 50;
+    //     // transform.ry = (ev.direction & Hammer.DIRECTION_HORIZONTAL) ? 1 : 0;
+    //     // transform.rx = (ev.direction & Hammer.DIRECTION_VERTICAL) ? 1 : 0;
+    //     // transform.angle = (ev.direction & (Hammer.DIRECTION_RIGHT | Hammer.DIRECTION_UP)) ? angle : -angle;
 
-        clearTimeout(timer);
-        timer = setTimeout(function () {
-            resetElement();
-        }, 300);
-        requestElementUpdate();
-    }
+    //     // clearTimeout(timer);
+    //     // timer = setTimeout(function () {
+    //     //     resetElement();
+    //     // }, 300);
+    //     // requestElementUpdate();
+    // }
 
-    function onTap(ev) {
-        transform.rx = 1;
-        transform.angle = 25;
+    // function onTap(ev) {
+    // 	console.log("tap");
+    //     // transform.rx = 1;
+    //     // transform.angle = 25;
 
-        clearTimeout(timer);
-        timer = setTimeout(function () {
-            resetElement();
-        }, 200);
-        requestElementUpdate();
-    }
+    //     // clearTimeout(timer);
+    //     // timer = setTimeout(function () {
+    //     //     resetElement();
+    //     // }, 200);
+    //     // requestElementUpdate();
+    // }
 
-    function onDoubleTap(ev) {
-        transform.rx = 1;
-        transform.angle = 80;
+    // function onDoubleTap(ev) {
+    // 	console.log("double tap");
+    //     // transform.rx = 1;
+    //     // transform.angle = 80;
 
-        clearTimeout(timer);
-        timer = setTimeout(function () {
-            resetElement();
-        }, 500);
-        requestElementUpdate();
-    }
+    //     // clearTimeout(timer);
+    //     // timer = setTimeout(function () {
+    //     //     resetElement();
+    //     // }, 500);
+    //     // requestElementUpdate();
+    // }
 
-    resetElement();
+  //  resetElement();
 
 
 }
