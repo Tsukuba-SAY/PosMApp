36db2478fbaea90ff2182d172e26d07d2e3d8e03
Yuji Imagawa <using923@gmail.com>
2014-11-07 13:04:20 +0900

    途中

--- PosMApp/www/js/postermap-function.js
+++ PosMApp/www/js/postermap-function.js
@@ -543,10 +543,139 @@ function windowManager () {
 	var $mapMain = $('#mapMain');
 	$mapMain.css("zoom",scale);
 
-	var mc = new Hammer($mapMain[0]);
+    var reqAnimationFrame = (function () {
+        return window[Hammer.prefixed(window, 'requestAnimationFrame')] || function (callback) {
+            window.setTimeout(callback, 1000 / 60);
+        };
+    })();
 
-	mc.on('pan',function (ev) {
-		console.log(ev);
-	})
+    console.log(reqAnimationFrame);
+
+    var el = document.querySelector("#mapMain");
+    console.log(el);
+
+    var START_X = Math.round((window.innerWidth - el.offsetWidth) / 2);
+    var START_Y = Math.round((window.innerHeight - el.offsetHeight) / 2);
+    console.log(START_X + "," + START_Y)
+
+    var ticking = false;
+    var transform;
+    var timer;
+
+    var mc = new Hammer.Manager(el);
+
+    mc.add(new Hammer.Pan({ threshold: 0, pointers: 0 }));
+
+    mc.add(new Hammer.Swipe()).recognizeWith(mc.get('pan'));
+    mc.add(new Hammer.Rotate({ threshold: 0 })).recognizeWith(mc.get('pan'));
+    mc.add(new Hammer.Pinch({ threshold: 0 })).recognizeWith([mc.get('pan'), mc.get('rotate')]);
+
+    mc.add(new Hammer.Tap({ event: 'doubletap', taps: 2 }));
+    mc.add(new Hammer.Tap());
+
+    mc.on("panstart panmove", onPan);
+    mc.on("rotatestart rotatemove", onRotate);
+    mc.on("pinchstart pinchmove", onPinch);
+    mc.on("swipe", onSwipe);
+    mc.on("tap", onTap);
+    mc.on("doubletap", onDoubleTap);
+
+
+    function resetElement() {
+        el.className = 'animate';
+        transform = {
+            translate: { x: START_X, y: START_Y },
+            scale: 1,
+            angle: 0,
+            rx: 0,
+            ry: 0,
+            rz: 0
+        };
+
+          }
+
+    function updateElementTransform() {
+        ticking = false;
+    }
+
+    function requestElementUpdate() {
+        if(!ticking) {
+            reqAnimationFrame(updateElementTransform);
+            ticking = true;
+        }
+    }
+
+    
+    function onPan(ev) {
+        el.className = '';
+        transform.translate = {
+            x: START_X + ev.deltaX,
+            y: START_Y + ev.deltaY
+        };
+
+        requestElementUpdate();
+    }
+
+    var initScale = 1;
+    function onPinch(ev) {
+        if(ev.type == 'pinchstart') {
+            initScale = transform.scale || 1;
+        }
+
+        el.className = '';
+        transform.scale = initScale * ev.scale;
+
+        requestElementUpdate();
+    }
+
+    var initAngle = 0;
+    function onRotate(ev) {
+        if(ev.type == 'rotatestart') {
+            initAngle = transform.angle || 0;
+        }
+
+        el.className = '';
+        transform.rz = 1;
+        transform.angle = initAngle + ev.rotation;
+        requestElementUpdate();
+    }
+
+    function onSwipe(ev) {
+        var angle = 50;
+        transform.ry = (ev.direction & Hammer.DIRECTION_HORIZONTAL) ? 1 : 0;
+        transform.rx = (ev.direction & Hammer.DIRECTION_VERTICAL) ? 1 : 0;
+        transform.angle = (ev.direction & (Hammer.DIRECTION_RIGHT | Hammer.DIRECTION_UP)) ? angle : -angle;
+
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+            resetElement();
+        }, 300);
+        requestElementUpdate();
+    }
+
+    function onTap(ev) {
+        transform.rx = 1;
+        transform.angle = 25;
+
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+            resetElement();
+        }, 200);
+        requestElementUpdate();
+    }
+
+    function onDoubleTap(ev) {
+        transform.rx = 1;
+        transform.angle = 80;
+
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+            resetElement();
+        }, 500);
+        requestElementUpdate();
+    }
+
+    resetElement();
+
 
 }
