cc70189fd782c3466fee4e19ae3f91bf75ab4ced
Shota Furuya <s1420693@u.tsukuba.ac.jp>
2014-11-12 14:30:06 +0900

    recognizeをmc.getにした

--- PosMApp/www/js/window-manager.js
+++ PosMApp/www/js/window-manager.js
@@ -27,9 +27,8 @@ function windowManager () {
     reset();
 
     var mc = new Hammer.Manager($("#mapFrame")[0]);
-    var pan = new Hammer.Pan();
-    mc.add(pan);
-    mc.add(new Hammer.Pinch()).recognizeWith(pan);
+    mc.add(new Hammer.Pan());
+    mc.add(new Hammer.Pinch()).recognizeWith(mc.get("pan"));
     mc.add(new Hammer.Tap({ event: 'doubletap', taps: 2 }));
 
     mc.on("panstart", function(ev) {
