372be90069a7f6b5dc3e4bcda70bbabb2d9b5e93
Shota Furuya <s1420693@u.tsukuba.ac.jp>
2014-11-21 15:18:08 +0900

    ログを保存するfunctionを実装

--- PosMApp/www/js/logdata.js
+++ PosMApp/www/js/logdata.js
@@ -1,11 +1,19 @@
-function saveLog(json) {
+var date = new Date();
 
+// ログを保存
+// json : データのJSONオブジェクト
+function saveLog(json) {
+	var uid = localStorage.getItem("uid");
+	if (uid === null) {
+		json["uid"] = uid;
+		localStorage.setItem(uid + "_" + date.getTime(), JSON.stringify(json));
+	}
 }
 
 function loadLog() {
-	var json;
+	var jsons = new Array();
 
-	return json;
+	return jsons;
 }
 
 function sendLog(json) {
