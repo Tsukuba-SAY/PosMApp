946a546dd62dc4c32caa609d36b8c3bf8f895174
Shota Furuya <s1420693@u.tsukuba.ac.jp>
2014-11-28 16:41:25 +0900

    mysqlに接続する部分を追加

--- PosMApp/www/php/savelog.php
+++ PosMApp/www/php/savelog.php
@@ -1,14 +1,20 @@
 <?php
-// Script for webhooks in GitHub and Slack
-// Author: Shota Furuya
-// Date: 2014/11/25 -
-
-// define("ADDRESS", "");
-// define("USER", "root");
-// define("PASSWORD", "");
-// define("SECRET_KEY", "");
+include "/etc/poslog_db_user_data.php";
 
 $json = file_get_contents("php://input");
 $data = json_decode($json);
+
+$mysqli = new mysqli($url, $user, $password, $db);
+
+if (mysqli_connect_error()) {
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	exit();
+}
+
+$mysqli->set_charset("utf-8");
+
+$stmt = $mysqli->prepare("INSERT INTO log VALUES (NULL, ?, ?)");
+
+
 
 ?>
