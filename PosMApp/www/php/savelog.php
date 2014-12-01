0ce8f1ce83ac24b0087ebd1305227adcbf447ae3
Shota Furuya <s1420693@u.tsukuba.ac.jp>
2014-12-01 23:17:39 +0900

    phpをPOST用に再修正

--- PosMApp/www/php/savelog.php
+++ PosMApp/www/php/savelog.php
@@ -2,6 +2,9 @@
 // DBのuserdataが置いてあるのでinclude
 include "/etc/poslog_db_user_data.php";
 
+$json = file_get_contents("php://input");
+$json_data = json_decode($json);
+
 $mysqli = new mysqli($url, $user, $password, $db);
 
 if (mysqli_connect_error()) {
@@ -14,8 +17,8 @@ $mysqli->set_charset("utf-8");
 $stmt = $mysqli->prepare("INSERT INTO log (id, uid, timestamp, data) VALUES (NULL, ?, ?, ?)");
 $stmt->bind_param("sss", $uid, $timestamp, $data);
 
-$uid = $_GET["uid"];
-$logdata = $_GET["logdata"];
+$uid = $json_data["uid"];
+$logdata = $json_data["logdata"];
 
 foreach ($logdata as $value) {	
 	$timestamp = $value["timestamp"];
@@ -27,6 +30,6 @@ $stmt->close();
 $mysqli->close();
 
 header('Content-Type: text/javascript; charset=utf-8');
-echo sprintf("callback()");
+echo sprintf($json_data);
 
 ?>
