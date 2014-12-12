fc44489f4f117029b240b8482575bb02eacc57fc
zouyimin <zouyimin88@gmail.com>
2014-12-12 10:46:49 +0900

    Add icon positon for deim2015 and change position_map Array from 0 to 117

--- PosMApp/www/js/posterdata_deim2014.js
+++ PosMApp/www/js/posterdata_deim2014.js
@@ -1,6 +1,6 @@
 // ポスターデータのJSONを置いておく（仮）
 
-var position_map = [12, 9, 6, 4, 11, 7, 5, 2, 0, 15, 16, 14, 13, 8, 10, 3, 1];
+var position_map = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117];
 
 var poster =
 [
@@ -99,8 +99,30 @@ var poster =
 {"id":93,"sessionid":"P3-2","title":"測域センサデータのリアルタイム可視化システム","abstract":"NO DATA","bookmark":0,"star":1},
 {"id":94,"sessionid":"P3-3","title":"グラフインタフェースにおけるクエリ操作に基づく検索結果予見システム","abstract":"NO DATA","bookmark":0,"star":1},
 {"id":95,"sessionid":"P3-4","title":"果物の販売促進のためのWebサイト構築","abstract":"NO DATA","bookmark":0,"star":1},
-{"id":96,"sessionid":"P3-5","title":"Analyzing Dissemination Processes of Mail-based Disaster Information Systems -A Case Study of Typhoon Man-yi in 2013-","abstract":"NO DATA","bookmark":0,"star":1}]
-;
+{"id":96,"sessionid":"P3-5","title":"Analyzing Dissemination Processes of Mail-based Disaster Information Systems -A Case Study of Typhoon Man-yi in 2013-","abstract":"NO DATA","bookmark":0,"star":1},
+{"id":97,"sessionid":"X3-4","title":"謎のX","abstract":"NO DATA","bookmark":0,"star":1},
+{"id":98,"sessionid":"X3-5","title":"謎のX","abstract":"NO DATA","bookmark":0,"star":1},
+{"id":99,"sessionid":"X3-6","title":"謎のX","abstract":"NO DATA","bookmark":0,"star":1},
+{"id":100,"sessionid":"X3-7","title":"謎のX","abstract":"NO DATA","bookmark":0,"star":1},
+{"id":101,"sessionid":"X3-8","title":"謎のX","abstract":"NO DATA","bookmark":0,"star":1},
+{"id":102,"sessionid":"X3-9","title":"謎のX","abstract":"NO DATA","bookmark":0,"star":1},
+{"id":103,"sessionid":"X3-10","title":"謎のX","abstract":"NO DATA","bookmark":0,"star":1},
+{"id":104,"sessionid":"X3-11","title":"謎のX","abstract":"NO DATA","bookmark":0,"star":1},
+{"id":105,"sessionid":"X3-12","title":"謎のX","abstract":"NO DATA","bookmark":0,"star":1},
+{"id":106,"sessionid":"X3-13","title":"謎のX","abstract":"NO DATA","bookmark":0,"star":1},
+{"id":107,"sessionid":"X3-14","title":"謎のX","abstract":"NO DATA","bookmark":0,"star":1},
+{"id":108,"sessionid":"X3-15","title":"謎のX","abstract":"NO DATA","bookmark":0,"star":1},
+{"id":109,"sessionid":"X3-16","title":"謎のX","abstract":"NO DATA","bookmark":0,"star":1},
+{"id":110,"sessionid":"X3-17","title":"謎のX","abstract":"NO DATA","bookmark":0,"star":1},
+{"id":111,"sessionid":"X3-18","title":"謎のX","abstract":"NO DATA","bookmark":0,"star":1},
+{"id":112,"sessionid":"X3-19","title":"謎のX","abstract":"NO DATA","bookmark":0,"star":1},
+{"id":113,"sessionid":"X3-20","title":"謎のX","abstract":"NO DATA","bookmark":0,"star":1},
+{"id":114,"sessionid":"X3-21","title":"謎のX","abstract":"NO DATA","bookmark":0,"star":1},
+{"id":115,"sessionid":"X3-22","title":"謎のX","abstract":"NO DATA","bookmark":0,"star":1},
+{"id":116,"sessionid":"X3-23","title":"謎のX","abstract":"NO DATA","bookmark":0,"star":1},
+{"id":117,"sessionid":"X3-24","title":"謎のX","abstract":"NO DATA","bookmark":0,"star":1},
+{"id":118,"sessionid":"X3-25","title":"謎のX","abstract":"NO DATA","bookmark":0,"star":1}
+];
 
 var author =
 
