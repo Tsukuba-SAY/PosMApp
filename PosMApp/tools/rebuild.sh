b989449e7a69e99290af7d99a44580ad7d70d14d
Shota Furuya <s1420693@u.tsukuba.ac.jp>
2015-01-14 23:17:29 +0900

    コマンドの実行方法を元に戻した

--- PosMApp/tools/rebuild.sh
+++ PosMApp/tools/rebuild.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
-/usr/bin/cd /PosMAppBuild
-/usr/bin/pwd
-/usr/bin/git pull
+cd /PosMAppBuild
+pwd
+git pull
 #git status
-/usr/bin/git branch --contains
-/usr/sbin/httpd -k restart
-/usr/bin/echo "httpd restarted"
-/usr/bin/echo "rebuild done!"
+git branch --contains
+httpd -k restart
+echo "httpd restarted"
+echo "rebuild done!"
 exit 0
