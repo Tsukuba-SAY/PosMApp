ce4ba87f4ebc5e476338bdad122df6710013bf52
Shota Furuya <s1420693@u.tsukuba.ac.jp>
2015-01-21 12:48:02 +0900

    ズーム率を戻すボタンのトリガーをclickにした

--- PosMApp/www/js/postermap-function-hammer.js
+++ PosMApp/www/js/postermap-function-hammer.js
@@ -13,7 +13,7 @@ function initHammer() {
     mc = new Hammer.Manager($("#mapFrame")[0]);
 
     $("#resetScaleButtonFrame").css("display", "none");
-    $("#resetScaleButton").on("touchstart", resetZoom);
+    $("#resetScaleButton").on("click", resetZoom);
     $(".posterfont").css("display", "none");
 
     var $mapMain = $("#mapMain");
