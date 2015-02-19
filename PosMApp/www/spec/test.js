0cdb52c0c7cef4238c359927e760684b1ebe45d6
zouyimin <zouyimin88@gmail.com>
2015-02-19 15:44:11 +0900

    発表リストの部分のテストの修正

--- PosMApp/www/spec/test.js
+++ PosMApp/www/spec/test.js
@@ -1397,7 +1397,7 @@ describe("発表リスト", function() {
 	});
 	// HACK: 直打ちしないで計算した方がいい
 	it("リストでセッションの情報がちゃんと表示されている", function() {
-		var sessionNum = "A1,B1,C1,D1,E1,F1,P1,A2,B2,C2,D2,E2,F2,P2,A3,B3,C3,D3,E3,F3,P3,A4,B4,C4,D4,E4,F4,P4,A5,B5,C5,D5,E5,F5,A6,B6,C6,D6,E6,F6,C7,D7,E7,F7,A8,B8,C8,D8,E8,F8";
+		var sessionNum = "A1,B1,C1,D1,E1,F1,G1,P1,A2,B2,C2,D2,E2,F2,G2,P2,A3,B3,C3,D3,E3,F3,G3,P3,A4,B4,C4,D4,E4,F4,G4,P4,A5,B5,C5,D5,E5,F5,G5,A6,B6,C6,D6,E6,F6,G6,C7,D7,E7,F7,G7,A8,B8,C8,D8,E8,F8,G8";
 		expect(sessionStorage.getItem("testSessionNum")).toEqual(sessionNum);
 	});
 });
