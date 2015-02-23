b53f042e492e13a71f97005c1bd5cb2f7f93d511
Shota Furuya <s1420693@u.tsukuba.ac.jp>
2015-02-23 11:51:39 +0900

    ログ収集許諾ダイアログでいいえを押すとリロードするたびにダイアログができる問題を修正

--- PosMApp/www/js/logdata-function.js
+++ PosMApp/www/js/logdata-function.js
@@ -2,9 +2,6 @@ $.fn.acceptCollectLog = function() {
 	$(this).on("click", function(e) {
 		console.log("collect log accept");
 
-		var uid = createUID();
-		localStorage.setItem("uid", uid);
-
 		// 現在時刻を最終送信日時とする
 		var date = new Date();
 		localStorage.setItem("log_last_sent", date.getTime().toString());
@@ -46,9 +43,14 @@ $.fn.selectUserCategory = function() {
 
 // 初期設定用
 function initUserData() {
+	// 固有識別IDが設定されていない場合は設定
+	if (localStorage.getItem("uid") === null) {
+		var uid = createUID();
+		localStorage.setItem("uid", uid);
+	} 
+
 	// ログ収集許諾フラグだけを折られた時でもUIDをリセットするのは仕様
-	if (localStorage.getItem("uid") === null ||
-		localStorage.getItem("accept_collect_log") === null) {
+	if (localStorage.getItem("accept_collect_log") === null) {
 		// デフォルト
 		var category = 1;
 		localStorage.setItem("category", category);
